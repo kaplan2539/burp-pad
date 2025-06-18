54110575b76618a91e3aeb1cda862b6d433c606d
kaplan2539 <kaplan2539@gmail.com>
2025-06-18 23:51:22 +0200

    change of course

--- src/main.py
+++ src/main.py
@@ -1,55 +1,101 @@
+import kivy
 from kivy.app import App
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.button import Button
-import sounddevice as sd
-import soundfile as sf
-import numpy as np
+from kivy.uix.label import Label
+from kivy.uix.label import Widget
+from kivy.core.window import Window
+from kivy.graphics import Rectangle
+from kivy.graphics import Color
+from kivy.clock import Clock
+from kivy.config import Config
+import random
 
-FILENAME = "recorded_audio.wav"
-SAMPLERATE = 44100  # Standard sample rate
-DURATION = 5  # seconds
+class Game(Widget):
+    def __init__(self,**kwargs) -> None:
+        super(Game,self).__init__(**kwargs)
+        self.init()
+        Clock.schedule_interval(self._tic,0.1)
 
-class AudioApp(App):
+    def _tic(self,dt):
+        self.update(dt)
+        self.draw(dt)
+
+    def init(self):
+        self.nx=40
+        self.ny=40
+        if Window.width >= Window.height:
+            self.w = self.h = int(Window.height / self.ny)
+            self.nx = int(Window.width / self.w)
+        else:
+            self.w = self.h = int(Window.width / self.nx)
+            self.ny = int(Window.height / self.h)
+        self.n = self.nx*self.ny
+        self.a=[]
+        self.b=[]
+        for i in range(0,self.n):
+            self.a.append(0)
+            self.b.append(0)
+        for i in range(0,self.n//10):
+            self.a[random.randint(0,self.n-1)]=1
+        self.draw(0)
+
+    def update(self,dt):
+        alive=0
+        for i in range(0,self.n):
+            (x,y) = self.pos(i)
+            neighbors = self.neighbors(x,y)
+            if self.a[i] == 0:
+                if neighbors==3:
+                    self.b[i] = 1
+                    alive += 1
+                else:
+                    self.b[i] = 0
+            else:
+                if neighbors==2 or neighbors==3:
+                    self.b[i] = 1
+                    alive+=1
+                else:
+                    self.b[i] = 0
+        c=self.a
+        self.a=self.b
+        self.b=c
+
+    def pos(self, i):
+        x =  i % self.nx
+        y =  i // self.nx 
+        return (x,y)
+
+    def get(self,x,y):
+        x = x % self.nx
+        y = y % self.ny
+        return self.a[y * self.nx + x]
+
+    def neighbors(self,x,y):
+        return self.get(x-1,y-1) + self.get(x,y-1) + self.get(x+1,y-1) \
+             + self.get(x-1,y) + self.get(x+1,y) \
+             + self.get(x-1,y+1) + self.get(x,y+1) + self.get(x+1,y+1)
+
+    def pixel_pos(self, i):
+        (x,y)=self.pos(i)
+        x *= self.w
+        y *= self.h
+        return (x,y)
+
+    def draw(self,dt):
+        self.canvas.clear()
+        with self.canvas:
+            Color(1,0,0,1)
+            for i in range(0,self.n):
+                if self.a[i]!=0:
+                    Rectangle(size=(self.w,self.h),pos=self.pixel_pos(i))
+
+class MyApp(App):
+
     def build(self):
-        layout = BoxLayout(orientation='vertical', padding=20, spacing=20)
+        #Config.fullscreen="auto"
+        return Game()
 
-        btn_record = Button(text="Record Sound")
-        btn_record.bind(on_press=self.record_audio)
+if __name__ == "__main__":
+    Window.fullscreen="auto"
+    Window.maximize()
+    MyApp().run()
 
-        btn_play = Button(text="Play Normal")
-        btn_play.bind(on_press=self.play_audio)
-
-        btn_double = Button(text="Play Double Speed")
-        btn_double.bind(on_press=self.play_audio_double_speed)
-
-        btn_full_speed = Button(text="Play Full Speed")
-        btn_full_speed.bind(on_press=self.play_audio)
-
-        layout.add_widget(btn_record)
-        layout.add_widget(btn_play)
-        layout.add_widget(btn_double)
-        layout.add_widget(btn_full_speed)
-
-        return layout
-
-    def record_audio(self, instance):
-        print("Recording...")
-        audio = sd.rec(int(SAMPLERATE * DURATION), samplerate=SAMPLERATE, channels=1)
-        sd.wait()
-        sf.write(FILENAME, audio, SAMPLERATE)
-        print("Recording saved.")
-
-    def play_audio(self, instance):
-        print("Playing at normal speed...")
-        data, fs = sf.read(FILENAME, dtype='float32')
-        sd.play(data, fs)
-        sd.wait()
-
-    def play_audio_double_speed(self, instance):
-        print("Playing at double speed...")
-        data, fs = sf.read(FILENAME, dtype='float32')
-        sd.play(data, int(fs * 2))
-        sd.wait()
-
-if __name__ == '__main__':
-    AudioApp().run()
